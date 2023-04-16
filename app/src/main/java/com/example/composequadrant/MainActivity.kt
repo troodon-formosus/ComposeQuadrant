ad4c9ba49d43e22c3b97eb4019f115cdc68df55f
Iuliia Dronova <jtyutereva@gmail.com>
2023-04-16 20:09:04 +0300

    initial commit

---
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.example.composequadrant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeQuadrantTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    QuadrantStructure()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun QuadrantStructure() {
+    Column(Modifier.fillMaxWidth()
+    ) {
+        Row(Modifier.weight(1f)){
+            InfoCard(
+                title = stringResource(R.string.green_card_title),
+                descriptionText = stringResource(R.string.green_card_description),
+                backgroundColor = Color.Green,
+                modifier = Modifier.weight(1f)
+            )
+
+            InfoCard(
+                title = stringResource(R.string.yellow_card_title),
+                descriptionText = stringResource(R.string.yellow_card_description),
+                backgroundColor = Color.Yellow,
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)){
+            InfoCard(
+                title = stringResource(R.string.cyan_card_title),
+                descriptionText = stringResource(R.string.cyan_card_description),
+                backgroundColor = Color.Cyan,
+                modifier = Modifier.weight(1f)
+            )
+
+            InfoCard(
+                title = stringResource(R.string.gray_card_title),
+                descriptionText = stringResource(R.string.gray_card_description),
+                backgroundColor = Color.Gray,
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun InfoCard(
+    title: String,
+    descriptionText: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp),
+        )
+
+        Text(
+            text = descriptionText,
+            textAlign = TextAlign.Justify,
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeQuadrantTheme {
+        QuadrantStructure()
+    }
+}
